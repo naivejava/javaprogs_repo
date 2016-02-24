8d0b7bce9bc983f4ab931fd79413b11736800871
naivejava <naivejava.gmail.com>
2016-02-24 12:38:06 +1100

    Another class to add and divide numbers was added.

---
+++ Tutorial 1/src/JavaKnowledge/divideTwoNumbers.java
@@ -0,0 +1,30 @@
+/* Note about Package: If you do not declare your own package, Java will put your classes
+ * and methods under the DEFAULT PACKAGE.
+ * 
+ * In this demo, I am going to use JUnit to test the two classes 
+ * 
+ * This program will also be checked-in in Git and will be uploaded to GitHub.
+ */
+
+package JavaKnowledge;       // This must be the first statement in the program
+
+public class divideTwoNumbers {
+
+	private int divNum(int x, int y){
+		
+		return (x/y);
+	}
+	
+	private int addNum (int x, int y){
+		
+		return (x + y);
+	}
+	
+		
+	public static void main(String[] args) {
+		
+		
+
+	}
+
+}
